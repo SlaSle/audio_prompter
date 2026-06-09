7b065a562acfb5074765efba01d1160b5818aa8e
SlaSle <kosiba1@wp.pl>
2026-06-09 19:55:04 +0200

    Add files via upload

---
+++ songs-imported.js
@@ -0,0 +1,257 @@
+window.extraDemoSongs = [
+  {
+    id: "cisza-jak-ta",
+    raw: `# Cisza jak ta
+@artist: Budka Suflera
+@bpm: 120
+@rhythm: Rytm 044
+@scroll: 1.00
+
+[Zwrotka 1]
+[C]Sza, cicho sza, czas na [Am]ciszę
+[C]Już oddech jej [Am]coraz bliżej
+[Dm]Tego naprawdę ci brak
+[F]Ona jedna prawdziwy ma [G]smak
+[C]Cisza jak ta, [G]mm...
+[C]Sza, cicho sza, zbliż się do [Am]niej
+[C]Gra, ledwie drga blady [Am]płomień
+[Dm]Podejdź i zanuż się w nią
+[F]Kryształową i czystą jej [G]toń
+[C]Zanuż do dna, mm...
+
+[Refren]
+[C]Bliżej i [Am]bliżej i bliżej i [F]bliżej masz do [G]niej
+[C]Ciszej [Am]i ciszej i ciszej i ciszej co [F]dnia [G]
+[C]Kończ, po co ten [G7sus4]ciągły [C]hałas [a]
+[C]Sam zdwoić go, [G7sus4]wciąż się [C]starasz [a]
+[d]Tak Cię [Asus4]uczyli od [d]lat
+[F]Tylko [G]krzykiem zdobywa się [G7]świat
+[C]A to nie tak, mm... , [G]nie tak
+[C]Sza, [G7sus4]cicho sza, [C]czas na [a]ciszę
+[C]Tę [G7sus4]którą w swym [C]sercu [a]słyszysz
+[d]Kiedyś [Asus4]śpiewało jak z [d]nut
+[F]Teraz [G]gładki i zimny [G7]jak lód
+[C]Smutny to cud, oo... [G]smutny cud
+
+[Refren 2 x 2]
+[C]Bliżej i [Am]bliżej i bliżej i [F]bliżej masz do [G]niej
+[C]Ciszej [Am]i ciszej i ciszej i ciszej co [F]dnia [G]
+[d]Podejdź i [Asus4]zanuż się w [d]nią
+[F]Kryształową i [G]czystą jej [G7]toń
+[C]Cisza jak ta, mm... [G]jak ta`
+  },
+  {
+    id: "czy-mnie-jeszcze-pamietasz",
+    raw: `# Czy mnie jeszcze pamiętasz
+@artist: Czesław Niemen
+@bpm: 70
+@rhythm: Rytm 034
+@tone: 016
+@scroll: 1.00
+
+[Zwrotka 1]
+[C]Czy [G]mnie [C]jeszcze [C7]pamiętasz
+[F]Dowód [G]na to mi [C]daj
+[F]Czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]Baju baj, [G7]baju [C]baj [C7]
+[F]Czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]baju baj, [G7]baju [C]baj [F] [C]
+[C]Czy mnie [G]jeszcze [C]pamiętasz [C7]
+[F]I może [G]wrócić chcesz [C]tu
+[F]Czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]zapomniałaś [G7]mnie już [C] [C7]
+[F]czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]Zapomniałaś [G7]mnie już [C] [F] [C]
+
+[Refren x 2]
+[G7]A gdy zechcesz [C]wrócić [Am]tu
+[Dm]Wracaj z [G7]uśmiechem [C]swym
+[G7]Choćbym inną kochał, [C]kochał [Am]już
+[D7]Będziesz tylko [G]Ty
+[C]Czy mnie [G]jeszcze [C]pamiętasz [C7]
+[F]I [G]pamiętasz ten [C]dzień
+[F]Czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]zapomniałaś [G7]już mnie [C] [C7]
+[F]czy jak [G7]inne [C]dziewczęta [Am]
+[Dm]Zapomniałaś [G7]już mnie [C] [F] [C]`
+  },
+  {
+    id: "fly-me-to-the-moon",
+    raw: `# Fly me to the moon
+@artist: Frank Sinatra
+@bpm: 100
+@rhythm: Rytm 034
+@tone: 016
+@scroll: 1.00
+
+[Info]
+Muzyka i słowa: Bart Howard, 1954
+Najbardziej znane wykonanie: Frank Sinatra, 1964
+Charakter utworu: swing / jazz standard
+
+[Wersja angielska]
+[Am7]Fly me to the [Dm7]moon
+[G7]Let me play among [Cmaj7]the stars
+[Fmaj7]Let me see what spring [Bm7b5]is like
+[E7]On Jupiter and [Am7]Mars
+[Dm7]In [G7]other words, [Cmaj7]hold my [A7]hand
+[Dm7]In [G7]other words, [Cmaj7]baby, [E7]kiss me
+[Am7]Fill my heart with [Dm7]song
+[G7]And let me sing [Cmaj7]forever more
+[Fmaj7]You are all I [Bm7b5]long for
+[E7]All I worship and [Am7]adore
+[Dm7]In [G7]other words, [Cmaj7]please be [A7]true
+[Dm7]In [G7]other words, I [Cmaj7]love you
+
+[Wersja polska]
+[Am7]Zabierz mnie na [Dm7]Księżyc,
+[G7]pośród gwiazd chcę [Cmaj7]tańczyć znów.
+[Fmaj7]Pozwól mi choć raz [Bm7b5]zobaczyć,
+[E7]jak wygląda wiosna [Am7]tam - na Marsie i wśród chmur.
+[Dm7]W innych [G7]słowach - [Cmaj7]podaj [A7]dłoń,
+[Dm7]w innych [G7]słowach - [Cmaj7]pocałuj [E7]mnie.
+[Am7]Niech mój głos dziś [Dm7]brzmi,
+[G7]gdy śpiewam tylko [Cmaj7]o tym, że
+[Fmaj7]tyś jedynym moim [Bm7b5]światem,
+[E7]tym, co kocham - dniem i [Am7]snem.
+[Dm7]W innych [G7]słowach - [Cmaj7]bądź mi [A7]bliski,
+[Dm7]w innych [G7]słowach - [Cmaj7]kocham cię.`
+  },
+  {
+    id: "modlitwa-breakout",
+    raw: `# Modlitwa
+@artist: Breakout
+@bpm: 140
+@rhythm: Rytm 044
+@tone: 147
+@scroll: 1.00
+
+[Info]
+Muzyka: Tadeusz Nalepa
+Tekst: Bogdan Loebl
+Album: Na drugim brzegu tęczy (1969)
+
+[Zwrotka 1]
+[Em]Wysłuchaj mojej pieśni, [H7]Panie,
+[C]Do Ciebie wznoszę dzisiaj [G]głos.
+[Em]Ty jesteś wszędzie, [C]wszystkim [Am]jesteś Ty,
+[H7]Lecz kamieniem nie bądź [Em]mi.
+[C]Do Ciebie pieśnią wołam, [H7]Panie,
+[C]Bo ponoć [D]wszystko możesz [G]dać,
+[Em]Więc błagam, daj mi [C]szansę jeszcze [Am]raz,
+[H7]Daj mi ją ostatni [Em]raz.
+[Em]Wystarczy, żebyś skinął [H7]ręką,
+[C]Wystarczy [D]jedna Twoja [G]myśl,
+[Em]A zacznę życie swoje [C]jeszcze [Am]raz,
+[H7]Więc o boski błagam [Em]gest.
+[Em]Do Ciebie pieśnią wołam, [H7]Panie,
+[C]Czy słyszysz mój [D]błagalny [G]głos?
+[Em]Raz jeszcze daj [C]mi od początku [Am]iść,
+[H7]Daj mi życie jeszcze [Em]raz.
+[C]Już nie zmarnuję ani [Em]chwili,
+[C]Bo dni [D]straconych gorycz [G]znam,
+[Em]Więc błagam, daj mi [C]szansę jeszcze [Am]raz,
+[H7]Daj mi ją ostatni [Em]raz.
+[Em]A jeśli życia dać nie [H7]możesz,
+[C]To spraw, bym [D]przeżył jeszcze [G]raz
+[Em]Tę miłość, która [C]już wygasła w [Am]nas,
+[H7]Spraw, bym przeżył ją jeszcze [Em]raz.
+[Em]Do Ciebie pieśnią wołam, [H7]Panie,
+[C]Do Ciebie [D]wznoszę dzisiaj [G]głos.
+[Em]Ty chlebem, ptakiem, [C]słońcem możesz [Am]być,
+[H7]Więc kamieniem nie bądź [Em]mi.`
+  },
+  {
+    id: "szczesliwej-drogi-juz-czas",
+    raw: `# Szczęśliwej drogi już czas
+@artist: Ryszard Rynkowski
+@bpm: 200
+@rhythm: Rytm 209
+@tone: 077
+@scroll: 1.00
+
+[Zwrotka 1]
+[Dm]Los Cię w drogę pchnął
+[A]I ukradkiem drwiąc się śmiał
+[Dm]Bo nadzieję [Bdim]dając Ci
+[Gm]Fałszywy klejnot dał
+[Dm]A Ty idąc w świat
+[A]Patrzysz w klejnot ten co dnia
+[Dm]Chociaż rozpacz już [Bdim]od lat
+[Gm]Wyziera z [D]jego [Am]dna
+[Em]Co dnia
+[Em]Na rozstaju dróg
+[B7]Gdzie przydrożny Chrystus stał
+[Em]Zapytałeś [C/G]dokąd iść
+[B7]Frasobliwą minę miał
+[Em]Przystanąłeś więc
+[B7]Z płaczem brzóz sprzymierzyć się
+[Em]I uronić [C/G]pierwszy raz
+[B7]W czerwone [D]wino [Am]łzę
+[Em]W wino łzę
+
+[Refren]
+[G]Szczęśliwej drogi już czas
+[D]Mapę życia w sercu masz
+[Am]Jesteś jak młody [Em]ptak
+[G]Głuchy jest los
+[D]Nadaremnie wzywasz go
+[Am]Bo Twój głos...
+[Em]Idziesz wiecznie sam
+[B7]I już nic nie zmieni się
+[Em]Poza tym, że raz [C/G]jest za
+[B7]Raz przed Tobą Twój cień
+[Em]Los Cię w drogę pchnął
+[B7]I ukradkiem drwiąc się śmiał
+[Em]Bo nadzieję, bo nadzieję [C/G]dając Ci
+[B7]Fałszywy [D]klejnot [Am]dał
+[Em]Tak chciał
+
+[Refren 2]
+[G]Szczęśliwej drogi już czas
+[D]Mapę życia w sercu masz
+[Am]Jesteś jak młody [Em]ptak
+[G]Głuchy jest los
+[D]Nadaremnie wzywasz go
+[Am]Bo Twój głos...`
+  },
+  {
+    id: "wspomnienie-niemen",
+    raw: `# Wspomnienie
+@artist: Czesław Niemen
+@bpm: 140
+@rhythm: Rytm 210
+@tone: 078
+@scroll: 1.00
+
+[Zwrotka 1]
+[F]Mimozami [Dm]jesień się [Gm7]zaczyna, [C7]
+[F]Złotawa, [Dm]krucha i [Gm7]miła. [C7]
+[F]To ty, to ty [Dm]jesteś ta [Gm7]dziewczyna, [C7]
+[F]Która do [Dm]mnie na ulicę [Gm7]wychodziła. [C7]
+[F]Od twoich [Dm]listów [Gm7]pachniało w [C7]sieni,
+[F]Gdym [Dm]wracał zdyszany [Gm7]ze szkoły, [C7]
+[F]A po [Dm]ulicach w [Gm7]lekkiej jesieni [C7]
+[F]Fruwały za mną [Dm]jasne [Gm7]anioły. [C7]
+
+[Interlude]
+[Bbm7]Uuu... [Eb7] [Fm7] [G7] [Db7] [C7sus4] [Fmaj7] [Db] [Caug]
+
+[Zwrotka 2]
+[F]Mimozami [Dm]zwiędłość [Gm7]przypomina [C7]
+[F]Nieśmiertelnik [Dm]żółty - [Gm7]październik. [C7]
+[F]To ty, to [Dm]ty, [Gm7]moja jedyna, [C7]
+[F]Przychodziłaś [Dm]wieczorem do [Gm7]cukierni. [C7]
+[F]Z przemodlenia, [Dm]z [Gm7]przeomdlenia [C7]senny,
+[F]W parku [Dm]płakałem [Gm7]szeptanymi [C7]słowy.
+[F]Młodzik z [Dm]chmurek [Gm7]prześwitywał [C7]jesienny,
+[F]Od [Dm]mimozy złotej - [Gm7]majowy. [C7]
+[F]Ach, [Dm]czułymi, [Gm7]przemiłymi [C7]snami
+[F]Zasypiałem z [Dm]nim gasnącym [Gm7]o [C7]poranku,
+[F]W snach [Dm]dawnymi bawiąc [Gm7]się [C7]wiosnami,
+[F]Jak tą złotą, [Dm]jak tą wonną [Gm7]wiąza----[C7]nką.
+
+[Outro]
+[Bbm7]Uuu... [Eb7] [Fm7] [G7] [Db7] [C7sus4] [Fmaj7]`
+  }
+];
